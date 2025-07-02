01364170d9bf006e9d7bf3718d6c22915ca114b8
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2025-07-02 16:27:07 +0530

    solved the issue of mobile botton nav

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -4,13 +4,16 @@ import TopNavbar from '@/components/admin/TopNavbar';
 
 export default function AdminLayout({ children }) {
 	return (
-		<div className='flex min-h-screen bg-[#F8F9FA]'>
+		<div className='flex min-h-screen flex-col bg-[#F8F9FA]'>
 			<Sidebar />
 			<div className='flex-1 flex flex-col'>
 				<TopNavbar />
-				<div className='flex-1 p-4 lg:p-8 pt-6 lg:pt-4 pb-8 lg:pb-8 lg:ml-64'>
+				<main className='flex-1 p-4 lg:p-8 pt-6 lg:pt-4 pb-20 lg:pb-8 lg:ml-64'>
 					{children}
-				</div>
+				</main>
+				<nav className='fixed bottom-0 left-0 w-full z-50 bg-white border-t md:hidden'>
+					{/* Place your mobile nav icons here */}
+				</nav>
 			</div>
 		</div>
 	);
