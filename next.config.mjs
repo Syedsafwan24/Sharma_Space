b984bcbc42955234721aca87a10b1d39a704cf6d
Mohammed <azaanpeshmam45@gmail.com>
2025-07-02 23:51:11 +0530

    chore: update Content Security Policy for improved security and development flexibility

--- next.config.mjs
+++ next.config.mjs
@@ -40,8 +40,7 @@ const nextConfig = {
 				headers: [
 					{
 						key: 'Content-Security-Policy',
-						value:
-							"default-src 'self'; img-src * data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none';",
+						value: `default-src 'self'; script-src 'self' ${process.env.NODE_ENV === 'development' ? "'unsafe-eval' 'unsafe-inline'" : ''}; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self'; object-src 'none'; media-src 'self'; connect-src 'self';`,
 					},
 					{ key: 'X-Frame-Options', value: 'DENY' },
 					{ key: 'Referrer-Policy', value: 'strict-origin' },
